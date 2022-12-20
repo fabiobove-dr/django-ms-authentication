28e452dd93b1d7f5a4723b8ce88baa29bebb0766
Fabio Bove <fabio.bove.dr@gmail.com>
2022-12-20 10:42:47 +0100

    Update settings.py

--- django_ms_authentication/django_ms_authentication/settings.py
+++ django_ms_authentication/django_ms_authentication/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'put_here_the_secret_key'
 SECRET_KEY = ''
-
+  
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
